02248b7744036fcb8d3d7fd8adb8d1a2c9dee02b
josse alexandre <alexandre.josse@epitech.eu>
2020-09-19 12:54:24 +0200

    sitemap robot SEO

---
+++ sitemap-routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+	   <Route path='/' />
+	   <Route path='/home' />
+	   <Route path='/showroom' />
+	   <Route path='/gallery' />
+	   <Route path='/contact' />
+	   <Route path='/impot' />
+	   <Route path='/map' />
+	   <Route path='/plan' />
+	   <Route path='/financement' />
+	   <Route path='/CheminéesBois' />
+	   <Route path='/CheminéesGaz' />
+	   <Route path='/Qualibois' />
+	   <Route path='/PoeleGaz' />
+	   <Route path='/PoeleBois' />
+	   <Route path='/PoelePellets' />
+	   <Route path='/CheminéesElectriques' />
+	   <Route path='/Aide' />
+	   <Route path='/Histoire' />
+	   <Route path='/Savoir' />
+    </Route>
+);
