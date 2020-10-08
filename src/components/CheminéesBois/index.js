cde01e521830de9f02df6ae73dcc43739914b30c
josse alexandre <alexandre.josse@epitech.eu>
2020-10-08 19:01:08 +0200

    logo

--- src/components/CheminéesBois/index.js
+++ src/components/CheminéesBois/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useMediaQuery } from 'react-responsive';
 import Header from "../Header/index";
 import Footer from "../Footer/index";
 import ChazelleLogo from "../Logo/chazelle";
@@ -13,6 +14,16 @@ import * as S from "./style";
 import Helmet from "react-helmet";
 
 const CheminéesBois = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: '(max-device-width: 1224px)'
+  })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
   return (
     <>
