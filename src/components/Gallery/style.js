cdfc4ea9b6414060829bb550b147a5ed81a52348
josse alexandre <alexandre.josse@epitech.eu>
2020-10-19 00:54:06 +0200

    fix real

--- src/components/Gallery/style.js
+++ src/components/Gallery/style.js
@@ -9,7 +9,7 @@ import WebFont from "webfontloader";
 
 export const TestDiv = styled.div`
   text-align: center;
-  position:relative;
+  position: relative;
   display: relative;
   height: 2500px;
   flex-direction: column;
